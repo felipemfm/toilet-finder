943790a4bb14dc57fdd6078ce452b4eb0ea47993
felipemfm <felipemfm@gmail.com>
2022-08-03 01:10:48 +0900

    feat: Add /test GET endpoint.

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+app.use(express.json());
 
 // add middlewares
 app.use(express.static(path.join(__dirname, './client/build')));
-app.use(express.static('./client/public'));
+
+app.get('/test', (req, res) => {
+  res.status(200).json({ status: 'success', message: 'From express' });
+});
 
 app.use((req, res, next) => {
   res.sendFile(path.join(__dirname, './client/build', 'index.html'));
 });
+
 module.exports = app;
