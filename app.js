0bbf3923e9aba3f38c9ee97ba7e4a883c5b28698
felipemfm <felipemfm@gmail.com>
2022-08-03 00:32:23 +0900

    feat: Add express static path for React app

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.use(express.json());
+// add middlewares
+app.use(express.static(path.join(__dirname, './client/build')));
+app.use(express.static('./client/public'));
 
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, './client/build', 'index.html'));
+});
 module.exports = app;
