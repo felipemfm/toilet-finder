94fb1a60e680f3b7a73e0e37aac76195104bfd08
joe-sacco <102517425+joe-sacco@users.noreply.github.com>
2022-08-03 16:16:55 +0900

    feat: Add knex db configuration

---
+++ db/knex.js
@@ -0,0 +1,6 @@
+module.exports = {
+    clients: 'pg', 
+    connection: {database: 'greenfield', user: 'Joe', password: null}, 
+    migrations: {directory: './data/migrations/'}, 
+    seeds: {directory: './data/seeds/'}
+};
