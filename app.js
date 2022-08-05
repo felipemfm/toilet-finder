d73aea06a49dac1a291fbdf0eff4d203fbef015d
felipemfm <felipemfm@gmail.com>
2022-08-05 19:33:49 +0900

    feat: Update API to use a REST interface.

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ const haversineDistance = require('haversine-distance');
 
 const app = express();
 app.use(express.json());
-const userLocation = { lat: 35.6581391, lng: 139.7277848 };
+
+let cors = require('cors');
+app.use(cors());
 
 // add middleware
 app.use(express.static(path.join(__dirname, './client/build'))); // create path to URL
 
-// Return n closest locations
+// Sort data for N closest locations
 const closestNLocations = (data, userLocation, n) => {
   data.map((el) => {
     el.distance = haversineDistance(userLocation, { lat: el.lat, lng: el.lng });
@@ -23,33 +25,17 @@ const closestNLocations = (data, userLocation, n) => {
   return data.slice(0, n);
 };
 
-// return top ten closest locations from db
-app.get('/getToilet', async (req, res) => {
-  // const userLat = req.query.lat;
-  // const userLng = req.query.lng;
-  const data = await db('locations').select('*');
+app.get('/api/v1/closest', async (req, res) => {
+  const userLocation = req.query.userLocation;
+  const limit = req.query.limit || 10;
+  const mode = req.query.mode || "";
 
-  res.status(200).send(closestNLocations(data, userLocation, 10));
-});
-
-app.get('/getPrivate', async (req, res) => {
-  // const userLat = req.query.lat;
-  // const userLng = req.query.lng;
-  const data = await db('locations')
-    .select('*')
-    .whereNotIn('type', ['publicToilet']);
-
-  res.status(200).send(closestNLocations(data, userLocation, 10));
-});
-
-app.get('/getPublic', async (req, res) => {
-  // const userLat = req.query.lat;
-  // const userLng = req.query.lng;
-  const data = await db('locations')
-    .select('*')
-    .whereIn('type', ['publicToilet']);
-
-  res.status(200).send(closestNLocations(data, userLocation, 10));
+  try {
+    const data = await db('locations').select('*');
+    res.status(200).send(closestNLocations(data, userLocation, limit));
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.use('/', (req, res, next) => {
