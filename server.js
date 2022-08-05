be7940d4212cd5956a0ef301b34e1e0e59d041e4
felipemfm <felipemfm@gmail.com>
2022-08-06 01:09:11 +0900

    refact: Update port from 3000 to 8000

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 const server = app.listen(PORT, () => {
   console.log(`App running on port ${PORT}....`);
